e9a65562c15295dab40441a287791dad2fb11e06
Suji V <Suji26262@gmail.com>
2026-02-16 13:52:37 +0530

    Fix PWA

--- vite.config.js
+++ vite.config.js
@@ -1,32 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import { VitePWA } from 'vite-plugin-pwa'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   plugins: [
     react(),
     VitePWA({
-      registerType: 'autoUpdate',
+      registerType: "autoUpdate",
+      injectRegister: "auto",
       manifest: {
-        name: 'Daily English App',
-        short_name: 'DailyWords',
-        start_url: '/',
-        display: 'standalone',
-        background_color: '#141e30',
-        theme_color: '#4caf50',
+        name: "Daily Words",
+        short_name: "Words",
+        start_url: "/",
+        display: "standalone",
+        background_color: "#121212",
+        theme_color: "#4caf50",
         icons: [
           {
-            src: '/icon-192.png',
-            sizes: '192x192',
-            type: 'image/png'
+            src: "/icon-192.png",
+            sizes: "192x192",
+            type: "image/png"
           },
           {
-            src: '/icon-512.png',
-            sizes: '512x512',
-            type: 'image/png'
+            src: "/icon-512.png",
+            sizes: "512x512",
+            type: "image/png"
           }
         ]
       }
     })
   ]
-})
+});
